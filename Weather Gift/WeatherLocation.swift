c6dd7c5ba7312e5bb19d270c0be8c0fee8b753b9
Alexander Falcone <falconal@bc.edu>
2020-03-09 00:12:28 -0400

    Ch. 7.2 Completed

---
+++ Weather Gift/WeatherLocation.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherLocation.swift
+//  Weather Gift
+//
+//  Created by Alexander Falcone on 3/8/20.
+//  Copyright © 2020 Alexander Falcone. All rights reserved.
+//
+
+import Foundation
+
+
+struct WeatherLocation {
+    var name: String
+    var latitutude: Double
+    var longitude: Double
+}
