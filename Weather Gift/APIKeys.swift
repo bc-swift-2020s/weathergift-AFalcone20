4062aef6ee6fc44046deca6f3a8b1d701613f59e
Alexander Falcone <falconal@bc.edu>
2020-04-05 13:48:56 -0400

    with .gitignore

---
+++ Weather Gift/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  Weather Gift
+//
+//  Created by Alexander Falcone on 4/5/20.
+//  Copyright © 2020 Alexander Falcone. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyCVc8eeyN2csa0qDgNj-gjH-8XaoXAj6fs"
+}
